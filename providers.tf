296abacda8b5457ac1661e159ede73c6d1f19a57
la-belle-femme <labellefemme2023@gmail.com>
2025-08-11 16:56:13 -0400

    Remove backend block from providers.tf for bootstrap workflow

--- providers.tf
+++ providers.tf
@@ -12,10 +12,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    # Backend configuration will be provided via backend config file
-    # or environment variables in GitHub Actions
-  }
+  # Backend configuration will be added dynamically by GitHub Actions
 }
 
 provider "aws" {
