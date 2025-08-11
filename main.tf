7c7984f0087bd29676c5b33c60a3835a19fadd82
la-belle-femme <labellefemme2023@gmail.com>
2025-08-11 17:17:42 -0400

    Fix S3 bucket and DynamoDB table naming - shorter names

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "random_id" "bucket_suffix" {
 
 # S3 bucket for Terraform state
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${var.cluster_name}-terraform-state-${random_id.bucket_suffix.hex}"
+  bucket = "eks-tfstate-${var.aws_region}-${random_id.bucket_suffix.hex}"
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 
 # DynamoDB table for Terraform state locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "${var.cluster_name}-terraform-locks"
+  name           = "eks-tfstate-locks-${random_id.bucket_suffix.hex}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
 
@@ -46,7 +46,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 
   tags = {
-    Name = "${var.cluster_name}-terraform-locks"
+    Name = "eks-tfstate-locks-${random_id.bucket_suffix.hex}"
   }
 }
 
